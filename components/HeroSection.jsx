8c8d886dc650e6b34a78a35c2c6638a4ac4185a1
Changnam Go <changnam.go@gmail.com>
2025-01-20 23:29:59 +0900

    이미지 교체

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -7,7 +7,7 @@ export default function HeroSection({data}) {
                 alt="Background"
                 className="absolute inset-0 object-cover w-full h-full"
                 height={1080}
-                src="https://images.pexels.com/photos/4050314/pexels-photo-4050314.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                src="/blackmotiv.jpg"
                 style={{
                 aspectRatio: "1920/1080",
                 objectFit: "cover",
